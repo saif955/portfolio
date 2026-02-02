1f04476006be99e0ffed5df322dd24b0f3db689c
Saifur Rahman <99525405+saif955@users.noreply.github.com>
2026-02-02 11:14:50 +0600

    Fix import path casing for Navbar to support case-sensitive filesystems

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
